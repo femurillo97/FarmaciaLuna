4f52f03de30009ff2fbf646459673ae2aaa941bd
fernandomurillo1997 <116420884+fernandomurillo1997@users.noreply.github.com>
2024-07-31 15:55:45 -0600

    modificacion del login

--- vista/login.php
+++ vista/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" type="text/css "href="css/style.css>
+    <link rel="stylesheet" type="text/css "href="../css/style.css">
     <link rel="stylesheet" type="text/css "href="../css/css/all.min.css>
 
     <title>Login</title>
@@ -17,7 +17,7 @@
     </div>
     <div class="contenido-login">
         <form action=>
-            <img src="../img/logo.png" alt="">
+            <img src="../img/doctor.png" alt="">  
             <h2>Farmacia</h2>
             <div class="input-div dni">
                 <div class="i">
