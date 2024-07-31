65788a7f638217cbe0aa44340cc6d4f4ccb3d50e
fernandomurillo1997 <116420884+fernandomurillo1997@users.noreply.github.com>
2024-07-31 15:22:34 -0600

    subida del proyecto

---
+++ vista/login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css "href="css/style.css>
+    <link rel="stylesheet" type="text/css "href="../css/css/all.min.css>
+
+    <title>Login</title>
+</head>
+<body>
+<img class="wave" src="../img/wave.png" alt="wave">
+<div class="contenedor">
+    <div class="img">
+        <img src="../img/bg.svg" alt="bg">
+    </div>
+    <div class="contenido-login">
+        <form action=>
+            <img src="../img/logo.png" alt="">
+            <h2>Farmacia</h2>
+            <div class="input-div dni">
+                <div class="i">
+                    <i class="fas fa-user"></i>
+                </div>
+                <div class="div">
+                    <h5>DNI</h5>
+                    <input type="text" class="input" name="dni">
+                </div>
+            </div>
+            <div class="input-div pass">
+                <div class="i">
+                    <i class="fas fa-lock"></i>
+                </div>
+                <div class="div">
+                    <h5>Contraseña</h5>
+                    <input type="password" class="input" name="pass">
+                </div>
+            </div>
+            <a href="#">¿Olvidaste tu contraseña?</a>
+            <input type="submit" class="btn" value="Iniciar Sesión">
+            <a href="registro.php">Registrarse</a>
+        </form>    
+
+    </body>
+</html>
